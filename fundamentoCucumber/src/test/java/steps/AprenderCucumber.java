8252c830c1fbfffa141408cc74db799e3d22b82e
RyanRafaelA <ryrafael461@gmail.com>
2025-10-01 20:21:46 -0300

    Cenario: Contador

--- fundamentoCucumber/src/test/java/steps/AprenderCucumber.java
+++ fundamentoCucumber/src/test/java/steps/AprenderCucumber.java
@@ -3,21 +3,34 @@ package steps;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
+import org.junit.Assert;
 
 public class AprenderCucumber {
+
     @Dado("que criei o arquivo corretamente")
-    public void queCrieiOArquivoCorretamente() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+    public void queCrieiOArquivoCorretamente() { }
+
     @Quando("executá-lo")
-    public void executáLo() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+    public void executáLo() { }
+
     @Então("o especificação deve finalizar com sucesso")
-    public void oEspecificaçãoDeveFinalizarComSucesso() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void oEspecificaçãoDeveFinalizarComSucesso() { }
+
+    private int contador = 0;
+
+    @Dado("que o valor do contador é {int}")
+    public void queOValorDoContadorÉ(Integer int1) {
+        contador = int1;
+    }
+
+    @Quando("eu incrementar em {int}")
+    public void euIncrementarEm(Integer int1) {
+        contador += int1
+    }
+
+    @Então("o valor do contador ser {int}")
+    public void oValorDoContadorSer(Integer int1) {
+        Assert.assertEquals(contador, (int) int1);
+
     }
 }
