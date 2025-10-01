4f6648371e11dd38fa8d69b3a8fbcc3e38a5f8c7
RyanRafaelA <ryrafael461@gmail.com>
2025-10-01 20:02:13 -0300

    Cenario inicial

---
+++ fundamentoCucumber/src/test/java/steps/AprenderCucumber.java
@@ -0,0 +1,23 @@
+package steps;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
+
+public class AprenderCucumber {
+
+    @Dado("que criei o arquivo corretamente")
+    public void que_criei_o_arquivo_corretamente() {
+
+    }
+
+    @Quando("executá-lo")
+    public void executá_lo() {
+
+    }
+
+    @Então("o especificação deve finalizar com sucesso")
+    public void o_especificação_deve_finalizar_com_sucesso() {
+
+    }
+}
