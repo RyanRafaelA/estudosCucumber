20427d21226ceb535ec55763ed7874cdd590853c
RyanRafaelA <ryrafael461@gmail.com>
2025-10-01 20:14:45 -0300

    Adicionando o CucumberOptions

--- fundamentoCucumber/src/test/java/runners/Runner.java
+++ fundamentoCucumber/src/test/java/runners/Runner.java
@@ -1,8 +1,16 @@
 package runners;
 
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = {"steps"},
+        features = "src/test/resources/features/aprender_cucumber.feature",
+        plugin = "pretty",
+        monochrome = true,
+        snippets = CucumberOptions.SnippetType.CAMELCASE
+)
 public class Runner {
 }
--- fundamentoCucumber/src/test/java/steps/AprenderCucumber.java
+++ fundamentoCucumber/src/test/java/steps/AprenderCucumber.java
@@ -5,19 +5,19 @@ import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
 
 public class AprenderCucumber {
-
     @Dado("que criei o arquivo corretamente")
-    public void que_criei_o_arquivo_corretamente() {
-
+    public void queCrieiOArquivoCorretamente() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @Quando("executá-lo")
-    public void executá_lo() {
-
+    public void executáLo() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-
     @Então("o especificação deve finalizar com sucesso")
-    public void o_especificação_deve_finalizar_com_sucesso() {
-
+    public void oEspecificaçãoDeveFinalizarComSucesso() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 }
