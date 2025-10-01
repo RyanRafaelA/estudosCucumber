0aec3cffc4f7fbd5cf45b4058052594bbc6ed435
RyanRafaelA <ryrafael461@gmail.com>
2025-10-01 20:49:46 -0300

    Cenario: Calculo de atraso

--- fundamentoCucumber/src/test/java/steps/AprenderCucumber.java
+++ fundamentoCucumber/src/test/java/steps/AprenderCucumber.java
@@ -5,6 +5,10 @@ import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
 import org.junit.Assert;
 
+import javax.swing.text.DateFormatter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class AprenderCucumber {
 
     @Dado("que criei o arquivo corretamente")
@@ -25,12 +29,38 @@ public class AprenderCucumber {
 
     @Quando("eu incrementar em {int}")
     public void euIncrementarEm(Integer int1) {
-        contador += int1
+        contador += int1;
     }
 
     @Então("o valor do contador ser {int}")
     public void oValorDoContadorSer(Integer int1) {
         Assert.assertEquals(contador, (int) int1);
-
     }
+
+    LocalDate entrega;
+    DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    @Dado("que o prazo é dia {string}")
+    public void queOPrazoÉDia(String dataInicial) {
+        entrega = LocalDate.parse(dataInicial, formato);
+    }
+
+    @Quando("a entrega atrasar em {int} dias")
+    public void aEntregaAtrasarEmDias(Integer adicionar) {
+        entrega = entrega.plusDays(adicionar);
+    }
+
+    @Quando("a entrega atrasar em {int} meses")
+    public void aEntregaAtrasarEmMeses(Integer adicionar) {
+        entrega = entrega.plusMonths(adicionar);
+    }
+
+    @Então("a entrega será efetuada em {string}")
+    public void aEntregaSeráEfetuadaEm(String dataEsperada) {
+        System.out.println(entrega.toString());
+        LocalDate dataEsperadaFormatada = LocalDate.parse(dataEsperada, formato);
+
+        Assert.assertEquals(dataEsperadaFormatada, entrega);
+    }
+
 }
--- fundamentoCucumber/src/test/resources/features/aprender_cucumber.feature
+++ fundamentoCucumber/src/test/resources/features/aprender_cucumber.feature
@@ -14,3 +14,13 @@ Cenario: Deve incrementar contador
   Dado que o valor do contador é 15
   Quando eu incrementar em 3
   Então o valor do contador ser 18
+
+Cenario: Deve calcular atraso no prazo de entrega
+  Dado que o prazo é dia "05/04/2018"
+  Quando a entrega atrasar em 2 dias
+  Então a entrega será efetuada em "07/04/2018"
+
+Cenario: Deve calcular atraso no prazo de entrega da China
+  Dado que o prazo é dia "05/04/2018"
+  Quando a entrega atrasar em 2 meses
+  Então a entrega será efetuada em "05/06/2018"
