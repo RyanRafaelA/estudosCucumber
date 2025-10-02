b766412cdfb3eef94884627fada307d7cd6c5c55
RyanRafaelA <ryrafael461@gmail.com>
2025-10-01 21:00:57 -0300

    Organizando o projeto bddTesteUnitarios

---
+++ bddTesteUnitarios/src/test/java/runners/Runner.java
@@ -0,0 +1,15 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = {"steps"},
+        features = "src/test/resources/features/alugar_filme.feature",
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
+        plugin = "pretty"
+)
+public class Runner {
+}
